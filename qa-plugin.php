f3960534e7323cf14ee89209dec0115453e53cea
sujik18 <sujithmenon2288@gmail.com>
2025-08-24 12:52:09 +0530

    Remove meta portion

--- qa-plugin.php
+++ qa-plugin.php
@@ -1,15 +1,4 @@
 <?php
-/*
-    Plugin Name: Activity Heatmap
-    Plugin URI: https://github.com/sujik18/q2a-activity-heatmap
-    Plugin Description: Displays a Cal-Heatmap visualization of user activity.
-    Plugin Version: 1.0
-    Plugin Date: 2025-08-15
-    Plugin Author: Sujith Kanakkassery
-    Plugin Author URI: https://gateoverflow.in/user/SUJITH%27
-    Plugin License: GPLv2
-    Plugin Minimum Question2Answer Version: 1.8
-*/
 
 if (!defined('QA_VERSION')) exit;
 
